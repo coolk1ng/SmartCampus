45c7ef4d03abe9bea4f15d2a6b8c9999000369d1
CodeSniper <codesniperrrrr@gmail.com>
2022-01-01 05:27:39 +0800

    搭建开发环境

---
+++ src/test/java/com/codesniper/smartcampus/SmartCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesniper.smartcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
